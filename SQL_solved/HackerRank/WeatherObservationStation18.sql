d0f768b0046423a55f84a87ba7dd8fcd629853b2
JeonDoGyun <wjsehrbs2632@naver.com>
2024-10-29 11:37:56 +0900

    1029

--- SQL_solved/HackerRank/WeatherObservationStation18.sql
+++ SQL_solved/HackerRank/WeatherObservationStation18.sql
@@ -3,12 +3,5 @@
 -- c : LAT_N의 MAX
 -- d : LONG_W의 MAX
 -- 맨하탄 거리 : x1-x2 절댓값 + y1-y2 절댓값
-select ROUND(abs(a-c)+abs(b-d), 4) as Manhattan_distance
-from (
-    select
-        min(LAT_N) as a, 
-        min(LONG_W) as b, 
-        max(LAT_N) as c, 
-        max(LONG_W) as d
-    from station
-) a
+SELECT ROUND(ABS(MIN(LAT_N)-MAX(LAT_N))+ABS(MIN(LONG_W)-MAX(LONG_W)), 4) AS Manhattan_distance
+FROM STATION
