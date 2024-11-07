d922609eb805d3ebaf42c8cb02702d6f4bd3b3af
JeonDoGyun <wjsehrbs2632@naver.com>
2024-11-07 21:36:11 +0900

    1107

---
+++ Baekjoon/Silver_5/1475_방번호.py
@@ -0,0 +1,14 @@
+number = input()
+array = [0] * 10
+
+for i in range(len(number)):
+    num = int(number[i])
+    if num == 6 or num == 9:
+        if array[6] <= array[9]:
+            array[6] += 1
+        else:
+            array[9] += 1
+    else:
+        array[num] += 1
+
+print(max(array))
