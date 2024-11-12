0cff0032864aebb753169bb1fe6d9d52a7dd0a87
JeonDoGyun <wjsehrbs2632@naver.com>
2024-11-12 23:54:52 +0900

    1112 10816 다른 방법으로 풀기

--- Baekjoon/Silver_4/10816_숫자카드2.py
+++ Baekjoon/Silver_4/10816_숫자카드2.py
@@ -1,15 +1,34 @@
+# n = int(input())
+# n_list = list(map(int, input().split()))
+# m = int(input())
+# m_list = list(map(int, input().split()))
+
+# array = [0] * 20000001
+# answer = []
+# for a in n_list:
+#     array[a+10000000] += 1
+
+# for b in m_list:
+#     answer.append(array[b+10000000])
+
+# for i in answer:
+#     print(i, end=' ')
+
+# 메모리 사용 줄이기 위해 dict() 사용하기
 n = int(input())
 n_list = list(map(int, input().split()))
 m = int(input())
 m_list = list(map(int, input().split()))
 
-array = [0] * 20000001
-answer = []
+array = dict()
 for a in n_list:
-    array[a+10000000] += 1
+    if a in array:
+        array[a] += 1
+    else:
+        array[a] = 1
 
 for b in m_list:
-    answer.append(array[b+10000000])
-
-for i in answer:
-    print(i, end=' ')
+    if b in array:
+        print(array[b], end=' ')
+    else:
+        print(0, end=' ')
